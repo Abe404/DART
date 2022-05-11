221d072210d0b77bc476f8cdea550880fde08aff
Abraham Smith <abe@abesmith.co.uk>
2022-05-11 10:22:19 +0200

    updates to install ants scripts

--- install_ants.sh
+++ install_ants.sh
@@ -1,8 +1,23 @@
+
 # For this to work you will need to have installed cmake.
 # Which can be done with
 # sudo apt-get install cmake
+
+
+# I also found that I had to run the following command:
+# git config --global url."https://".insteadOf git://
+# Based on:
+# https://github.com/ANTsX/ANTs/issues/653#issuecomment-435957947
+# As the install script was cloning other repos in a way that is no longer supported.
+
 workingDir=${PWD}
 git clone https://github.com/ANTsX/ANTs.git
+cd ANTs
+# lets pin this to a specific commit - I found that the latest version had problems with the install process.
+git reset --hard 1fcc33df8299d6d8480ab67211d582202008eef6
+
+cd ..
+
 mkdir build install
 cd build
 cmake \
